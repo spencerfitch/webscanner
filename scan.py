c5a6100aeb84a0493da6966f1ccaa842844db8d4
Spencer Fitch <SpencerFitch2022@u.northwestern.edu>
2020-11-14 20:24:08 -0600

    Initial commandline argument and file reading

--- scan.py
+++ scan.py
@@ -6,4 +6,23 @@
 #
 # scan.py
 
+import sys
+import time
 
+# Check for command line argument
+if len(sys.argv) != 3:
+    sys.stderr.write("scan.py requires 2 arguments: input_file.txt and output_file.json \n")
+    sys.exit(10)
+
+input_file = open(sys.argv[1], "r")
+output_file = open(sys.argv[2], "w")
+
+websites = []
+
+for line in input_file:
+    websites.append(line.split('\n')[0])
+
+print(websites)
+
+input_file.close()
+output_file.close()
