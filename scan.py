622eda47ac792fe391ba495382b7914d0b4827c8
Spencer Fitch <SpencerFitch2022@u.northwestern.edu>
2020-11-15 14:15:45 -0600

    Updated DNS resolver list to remove bad IP

--- scan.py
+++ scan.py
@@ -16,12 +16,14 @@ import json
 import subprocess
 
 
-
+'''
 dns_resolvers = ['208.67.222.222', '1.1.1.1', '8.8.8.8', '8.26.56.26', '9.9.9.9', 
                  '64.6.65.6', '13.239.157.177', '91.239.100.100', '185.228.168.168', 
                  '77.88.8.7', '156.154.70.1', '198.101.242.72', '176.103.130.130']
-
-
+'''
+dns_resolvers = ['208.67.222.222', '1.1.1.1', '8.8.8.8', '8.26.56.26', '9.9.9.9', 
+                 '64.6.65.6', '91.239.100.100', '185.228.168.168', 
+                 '77.88.8.7', '156.154.70.1', '198.101.242.72', '176.103.130.130']
 def get_ip_addresses(website, ip_type):
     ip_addresses = []
 
@@ -43,10 +45,12 @@ def get_ip_addresses(website, ip_type):
             continue    
             
         for line in (split_result[1]).split('\n'):
-            if line.split(': ')[0] == "Address":
-                ip_addresses.append(line.split(': ')[1])
+            split_line = line.split(': ')
+            if (split_line[0] == "Address"):
+                if (split_line[1] not in ip_addresses):
+                    ip_addresses.append(split_line[1])
 
-    return ip_addresses
+    return ip_addresses.sort()
 
 
 
